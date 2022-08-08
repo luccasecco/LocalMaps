5d4fe0cac7cd9cf1eb6b5d0ee3f4cdce7f030fb2
luccasecco <luccasecco@hotmail.com>
2022-08-08 09:23:38 -0300

    add mapa e lista de categorias

--- app/src/Home/index.tsx
+++ app/src/Home/index.tsx
@@ -1,13 +1,44 @@
-import {View, Text, SafeAreaView} from 'react-native'
+import { View, Text, SafeAreaView, FlatList, TouchableOpacity, Image } from 'react-native'
 import { styles } from './styles'
+import MapView, { Marker } from 'react-native-maps'
 import React from 'react'
+import { categories } from './categories'
 
 export function Home() {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.headerContainer}>
-      <Text> Bem vindo </Text>
-      <Text> Encontre no mapa um ponto do comércio local </Text>  
+      <Text style={styles.title}> Bem vindo </Text>
+      <Text style={styles.subTitle}> Encontre no mapa um ponto do comércio local </Text>  
+      </View>
+
+      <MapView style={styles.map}>
+        <Marker coordinate={{
+          latitude: 0,
+          longitude: 0,
+        }}/>
+      </MapView>
+
+      <View style={styles.categoryContainer}>
+        <FlatList 
+          data={categories}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          ItemSeparatorComponent={() => <View style={{width: 10}}></View>} //
+          contentContainerStyle={{
+            alignItems: 'center',
+          }}
+          renderItem={({item}) => (
+            <TouchableOpacity
+            style={styles.categoryItem}
+              key={item.key}
+            >
+                <Image  style={styles.categoryImage} source={item.image}/>
+                <Text style={styles.categoryText}>{item.label}</Text>
+
+            </TouchableOpacity>
+          )}
+        />
       </View>
     </SafeAreaView>
   )
--- app/src/Home/styles.ts
+++ app/src/Home/styles.ts
@@ -8,5 +8,38 @@ export const styles = StyleSheet.create({
   headerContainer: {
     padding: 20,
     paddingTop: Platform.OS === 'android' ? 50 : 0
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '400',
+    color: '#3221531'
+  },
+  subTitle: {
+    fontSize: 14,
+    fontWeight: '400',
+    color: '#6C6C80'
+  },
+  map: {
+    flex: 1,
+  },
+  categoryContainer: {
+    padding: 10,
+  },
+  categoryItem: {
+    height: 110,
+    backgroundColor: '#f0f0f5',
+    width: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10
+  },
+  categoryImage: {
+    width: 50,
+    height: 50,
+  },
+  categoryText: {
+    textAlign: 'center',
+    color: '#6C6C80'
   }
+
 })
